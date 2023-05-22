55e9cbc093dc9a36f31e5a52287d1dd98a72ec65
alifiverr <alighazanfer@outlook.com>
2023-05-22 10:17:44 +0500

    ltc added

--- LTC1dBot.py
+++ LTC1dBot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## BTC 4H with 50$
+# ## ltc 4H with 50$
 
 # In[1]:
 
@@ -18,26 +18,26 @@ time.sleep(5)
 
 
 # Define the time periods for the moving averages and the Bollinger Bands
-length1 = 4
+length1 = 1
 length2 = 26
-bbPeriod = 33
+bbPeriod = 37
 bbStdDev = 1
 
 # Define RSI configuration
-rsiLength = 18
+rsiLength = 12
 dailyRSI = 48
 
 # Define stop loss in percentage
-stop_loss = 5.3
+stop_loss = 8.0
 
 # Define trading parameters
-symbol = 'BTC/USDT'
-usdt_amount = 75
+symbol = 'LTC/USDT'
+usdt_amount = 50
 timeframe = '1d'
 rsi_tf = '1d'
 
-tradesfile = "btc1D_trades.csv"
-logfile = "btc1D.csv"
+tradesfile = "ltc1D_trades.csv"
+logfile = "ltc1D.csv"
 
 
 # In[4]:
@@ -58,13 +58,13 @@ with open('qty.json', 'r') as f:
 pos = json.loads(json_pos)
 qty = json.loads(json_qty)
 
-in_position = pos['btc1d']
+in_position = pos['ltc1d']
 
 # In[5]:
 
 
 size = calculate_order_size(symbol, usdt_amount)
-qty = qty['btc1d']
+qty = qty['ltc1d']
 
 
 # cronjob code
@@ -87,20 +87,20 @@ try:
     if signal == True and not in_position:
         # Place buy order
         buyId = place_buy_order(symbol, size)
-        in_position = update_dict_value('pos.json', 'btc1d', True)
+        in_position = update_dict_value('pos.json', 'ltc1d', True)
         print(df.iloc[-2:-1])
         buyprice = float(buyId['info']['fills'][0]['price'])
         qty = float(buyId['info']['origQty'])
-        qty = update_dict_value('qty.json', 'btc1d', qty)
+        qty = update_dict_value('qty.json', 'ltc1d', qty)
         buycsv(df, buyprice, tradesfile)
         print(f'Buy order placed for {symbol} at {buyprice}')
 
     elif df['sell'][-1] == True and in_position:
         # Place sell order
         sellId = place_sell_order(symbol, qty)
-        in_position = update_dict_value('pos.json', 'btc1d', False)
+        in_position = update_dict_value('pos.json', 'ltc1d', False)
         sellprice = float(sellId['info']['fills'][0]['price'])
-        buyprice = read_buyprice()
+        buyprice = read_buyprice(tradesfile)
         profit = ((sellprice - buyprice) / buyprice- 0.002) * 100
         sellcsv(df, buyprice, sellprice, tradesfile)
         print(f'Sell order placed for {symbol} at {sellprice}, Profit: {profit:.2f}%')
@@ -109,9 +109,9 @@ try:
     elif in_position and (df['close'][-1] / read_buyprice() - 1) * 100 < -stop_loss/100:
         # Place sell order
         sellId = place_sell_order(symbol, qty)
-        in_position = update_dict_value('pos.json', 'btc1d', False)
+        in_position = update_dict_value('pos.json', 'ltc1d', False)
         sellprice = float(sellId['info']['fills'][0]['price'])
-        buyprice = read_buyprice("btcTrades")
+        buyprice = read_buyprice(tradesfile)
         profit = ((buyprice - sellprice) / buyprice- 0.002) * 100
         sellcsv(df, buyprice, sellprice, tradesfile)
         print(f'Sell order placed for {symbol} at {sellprice}, Profit: {profit:.2f}%')
--- LTC4hBot.py
+++ LTC4hBot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## BTC 4H with 50$
+# ## LTC 4H with 50$
 
 # In[1]:
 
@@ -18,26 +18,26 @@ time.sleep(5)
 
 
 # Define the time periods for the moving averages and the Bollinger Bands
-length1 = 5
-length2 = 26
-bbPeriod = 32
+length1 = 3
+length2 = 25
+bbPeriod = 38
 bbStdDev = 1
 
 # Define RSI configuration
-rsiLength = 18
-dailyRSI = 48
+rsiLength = 12
+dailyRSI = 50
 
 # Define stop loss in percentage
 stop_loss = 5.7
 
 # Define trading parameters
-symbol = 'BTC/USDT'
+symbol = 'LTC/USDT'
 usdt_amount = 50
 timeframe = '4h'
 rsi_tf = '1d'
 
-tradesfile = "btc4H_trades.csv"
-logfile = "btc4H.csv"
+tradesfile = "ltc4H_trades.csv"
+logfile = "ltc4H.csv"
 
 
 # In[4]:
@@ -58,13 +58,13 @@ with open('qty.json', 'r') as f:
 pos = json.loads(json_pos)
 qty = json.loads(json_qty)
 
-in_position = pos['btc4h']
+in_position = pos['ltc4h']
 
 # In[5]:
 
 
 size = calculate_order_size(symbol, usdt_amount)
-qty = qty['btc4h']
+qty = qty['ltc4h']
 
 # cronjob code
  
@@ -86,18 +86,18 @@ try:
     if signal == True and not in_position:
         # Place buy order
         buyId = place_buy_order(symbol, size)
-        in_position = update_dict_value('pos.json', 'btc4h', True)
+        in_position = update_dict_value('pos.json', 'ltc4h', True)
         print(df.iloc[-2:-1])
         buyprice = float(buyId['info']['fills'][0]['price'])
         qty = float(buyId['info']['origQty'])
-        qty = update_dict_value('qty.json', 'btc4h', qty)
+        qty = update_dict_value('qty.json', 'ltc4h', qty)
         buycsv(df, buyprice, tradesfile)
         print(f'Buy order placed for {symbol} at {buyprice}')
 
     elif df['sell'][-1] == True and in_position:
         # Place sell order
         sellId = place_sell_order(symbol, qty)
-        in_position = update_dict_value('pos.json', 'btc4h', False)
+        in_position = update_dict_value('pos.json', 'ltc4h', False)
         sellprice = float(sellId['info']['fills'][0]['price'])
         buyprice = read_buyprice(tradesfile)
         profit = ((sellprice - buyprice) / buyprice- 0.002) * 100
@@ -108,7 +108,7 @@ try:
     elif in_position and (df['close'][-1] / read_buyprice(tradesfile) - 1) * 100 < -stop_loss/100:
         # Place sell order
         sellId = place_sell_order(symbol, qty)
-        in_position = update_dict_value('pos.json', 'btc4h', False)
+        in_position = update_dict_value('pos.json', 'ltc4h', False)
         sellprice = float(sellId['info']['fills'][0]['price'])
         buyprice = read_buyprice(tradesfile)
         profit = ((buyprice - sellprice) / buyprice- 0.002) * 100
